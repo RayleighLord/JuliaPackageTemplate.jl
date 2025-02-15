189f3201ad4b2503ea0132c869a69b885b11bcc5
Documenter.jl <documenter@juliadocs.github.io>
2025-02-15 18:59:33 +0000

    build based on b58d0ba

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuliaPackageTemplate","category":"page"},{"location":"#JuliaPackageTemplate","page":"Home","title":"JuliaPackageTemplate","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JuliaPackageTemplate.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuliaPackageTemplate]","category":"page"}]
+}
